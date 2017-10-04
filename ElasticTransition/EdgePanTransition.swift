d1878a5b0b87cff543ba5f094963b70f72843aa5
Livio Gamassia <livio@lidmi.com>
2017-10-04 11:01:36 +0200

    Fix popviewcontroller for iOS 11

--- ElasticTransition/EdgePanTransition.swift
+++ ElasticTransition/EdgePanTransition.swift
@@ -90,10 +90,10 @@ public class EdgePanTransition: NSObject, UIViewControllerAnimatedTransitioning,
 
     if(!presenting && finished || presenting && !finished){
       frontView.removeFromSuperview()
-      backView.layer.transform = CATransform3DIdentity
 
       backViewController.viewDidAppear(true)
     }
+    backView.layer.transform = CATransform3DIdentity
 
     dragPoint = CGPoint.zero
     currentPanGR = nil
